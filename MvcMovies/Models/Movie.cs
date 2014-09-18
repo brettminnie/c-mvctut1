d0d076fddf5a3d4cd09de4aab6ee279bb1369214
brettminnie <github@bdb-studios.co.uk>
2014-09-18 20:27:42 +0100

    Tutorial 4 - Models

---
+++ MvcMovies/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovies.Models
+{
+    public class Movie
+    {
+        protected int Id { get; set; }
+
+        protected string Title { get; set; }
+
+        protected DateTime ReleaseDate { get; set; }
+
+        protected string Genre { get; set; }
+
+        protected decimal Price { get; set; }
+    }
+}
